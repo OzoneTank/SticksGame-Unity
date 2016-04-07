8ee8b8d33ae6bdadcdd4615242b876388d0c1a2b
OzoneTank <wates.cole@gmail.com>
2016-04-06 21:15:01 -0400

    initial game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public GameObject stick;
+	private GameObject currentStick;
+	private List<GameObject> sticks = new List<GameObject>();
+	public float playerOffsetHeight = 2.0f;
+	public Vector3 stickOffset = new Vector3 (2, 0, 0);
+	private float stickRotation = 0;
+	public float startStickRotationSpeed = 30f;
+	private float stickRotationSpeed = 30f;
+	public float stickRotationSpeedDelta = 0.5f;
+
+	public int numOfSticksToSwitchColor = 10;
+	public int numOfSticksSinceColorChange = 0;
+
+	public Text score;
+
+	private enum GameState {
+		Start,
+		Generating,
+		Holding,
+		Dropping,
+		GameOver
+	}
+
+	private GameState gameState = GameState.Generating;
+
+	void Start() {
+		sticks.AddRange (GameObject.FindGameObjectsWithTag ("Stick"));
+		stickRotationSpeed = startStickRotationSpeed;
+	}
+
+	public void Restart() {
+		SceneManager.LoadScene ("MainScene");
+	}
+	// Update is called once per frame
+	void Update () {
+		switch (gameState) {
+		case GameState.Generating:
+			GenerateStick ();
+			break;
+		case GameState.Holding:
+			RotateCurrentStick ();
+			if (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump") ||
+				Input.touchCount > 0) {
+				DropStick ();
+			}
+			break;
+		}
+		MoveOverHighestStick ();
+	}
+
+	private void RotateCurrentStick() {
+		stickRotation += stickRotationSpeed * Time.deltaTime;
+		stickRotation %= 360;
+
+		currentStick.transform.RotateAround (transform.position, Vector3.up, stickRotationSpeed * Time.deltaTime);
+	}
+
+	private void DropStick() {
+		currentStick.GetComponent<Rigidbody> ().isKinematic = false;
+		gameState = GameState.Dropping;
+	}
+
+	public void Continue() {
+		sticks.Add (currentStick);
+		gameState = GameState.Generating;
+		stickRotationSpeed += stickRotationSpeedDelta;
+		numOfSticksSinceColorChange++;
+		if (numOfSticksSinceColorChange > numOfSticksToSwitchColor) {
+			numOfSticksSinceColorChange = 0;
+			GameObject.Find ("Pedestal").GetComponent<Pedestal> ().GenerateNewColors ();
+		}
+		score.text = "" + (sticks.Count - 2);
+	}
+
+	public void GameOver() {
+		gameState = GameState.GameOver;
+	}
+
+	private void GenerateStick() {
+		Vector3 pos = transform.position + stickOffset;
+
+		currentStick = Instantiate (stick, pos, transform.rotation) as GameObject;
+		currentStick.transform.RotateAround (transform.position, Vector3.up, stickRotation);
+
+		currentStick.GetComponent<Rigidbody> ().isKinematic = true;
+		gameState = GameState.Holding;
+	}
+
+	private void MoveOverHighestStick() {
+
+		float maxY = 0;
+		for(int i = 0; i < sticks.Count; i++) {
+			float y = sticks [i].transform.position.y;
+
+			if (y > maxY) {
+				maxY = y;
+			}
+		}
+
+		Vector3 newPos = new Vector3 (0, maxY + playerOffsetHeight, 0);
+		transform.position = Vector3.Lerp (transform.position, newPos, Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Stick.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stick : MonoBehaviour {
+
+	public bool canTouchPedestal = false;
+	private bool hasCollided = false;
+
+	void Start() {
+		gameObject.GetComponent<Renderer> ().material.color = new Color(
+			Random.Range (0, 1.0f),
+			Random.Range (0, 1.0f),
+			Random.Range (0, 1.0f)
+		);
+	}
+	void Update () {
+		if (transform.position.y < 0) {
+			GameOver ();
+		}
+	}
+
+	private void GameOver(){
+		GameObject.Find ("Player").GetComponent<Player> ().GameOver ();
+	}
+
+	private void Continue() {
+		GameObject.Find ("Player").GetComponent<Player> ().Continue ();
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (!canTouchPedestal) {
+			if (collision.gameObject.tag == "Pedestal") {
+				GameOver ();
+			} else if (!hasCollided) {
+				Continue ();
+			}
+		}
+		hasCollided = true;
+	}
+}
